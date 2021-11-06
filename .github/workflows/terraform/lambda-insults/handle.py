7a8357d36bb5a20ce98e0431a3b18da9b0eeb2de
Zach Mowrey <zbmowrey@gmail.com>
2021-11-06 10:30:32 -0400

    Bootstrapping should only require slack url to be set in dynamo now

--- .github/workflows/terraform/lambda-insults/handle.py
+++ .github/workflows/terraform/lambda-insults/handle.py
@@ -17,6 +17,8 @@ boto_config = Config(
     }
 )
 
+# TODO - insults#slack by default.
+
 dynamodb = boto3.client('dynamodb', config=boto_config)
 ssm = boto3.client('ssm', config=boto_config)
 
@@ -28,18 +30,19 @@ param = ssm.get_parameter(
 )
 
 if "Parameter" not in param or "Value" not in param["Parameter"]:
-    raise "Missing the %s parameter" % table_key
+    raise Exception("Missing the %s parameter" % table_key)
 
 table = param["Parameter"]["Value"]
+
 
 # TODO this should fetch (and create) a map of Slack URLs so we can support multiple
 # webhooks/character quotations.
 
 def get_slack_url():
-    return get_value('S', "insults#slack")
+    return get_value('S', "insults#slack", 'S')
 
 
-def get_value(key_type, key_value):
+def get_value(key_type, key_value, value_type):
     i = dynamodb.get_item(
         TableName=table,
         Key={
@@ -50,14 +53,16 @@ def get_value(key_type, key_value):
     )
 
     if "Item" in i and "v" in i["Item"]:
-        return i["Item"]["v"][key_type]
+        return i["Item"]["v"][value_type]
     return None
+
 
 # TODO - insults#character#messages#%s - allow us to get a message for a specific character.
 
 
 def get_message(index):
-    return get_value("S", "insults#messages#%s" % index)
+    return get_value("S", "insults#messages#%s" % index, 'S')
+
 
 # TODO - figure out how to iterate ALL messages or just messages for each character.
 
@@ -78,26 +83,30 @@ def get_iterator():
     if "Item" in i:
         return int(i["Item"]["v"]["N"])
     else:
-        print("Need to create the iterator, it disappeared.")
-        return 1
+        reset_iterator()
+        return 0
+
 
 # TODO - implement multiple iterators as above - this will need to target specific iterators.
 
 def reset_iterator():
-    dynamodb.update_item(
+    put_item('S', iterator_key, 'N', "0")
+
+
+def put_item(key_type, key, message_type, message):
+    response = dynamodb.put_item(
         TableName=table,
-        Key={
+        Item={
             'k': {
-                'S': iterator_key
-            }
-        },
-        UpdateExpression="SET v = :val",
-        ExpressionAttributeValues={
-            ':val': {
-                'N': "0"
+                key_type: key
+            },
+            'v': {
+                message_type: message
             }
         }
     )
+    return response
+
 
 # TODO - implement multiple iterators as above - this will need to target specific iterators.
 
@@ -117,21 +126,13 @@ def increment_iterator():
         }
     )
 
+
 # TODO - implement multiple iterators as above - this will need to target specific iterators.
 
+
 def put_message(key, message):
-    response = dynamodb.put_item(
-        TableName=table,
-        Item={
-            'k': {
-                'S': key
-            },
-            'v': {
-                'S': message
-            }
-        }
-    )
-    return response
+    put_item('S', key, 'S', message)
+
 
 # TODO - fetch lines for a given character or loop through all files, creating quotes for all characters.
 
@@ -149,30 +150,34 @@ def create_messages():
         put_message(dynamo_key, message)
 
     # Tells us how many messages exist, so we know when to reset the loop.
-    put_message("insults#count", i)
+    put_item('S', "insults#count", 'N', str(i))
+
 
 # TODO - look for a parameter which matches a character name (see the map, at top of file).
 # TODO - if present, pick a quote from that character. Otherwise, pick a character at random
-# TODO - and trigger the next message for that one. 
+# TODO - and trigger the next message for that one.
 
 def handle(event, context):
-    body = dict(urlparse.parse_qsl(base64.b64decode(str(event['body'])).decode('ascii')))
+    # body = dict(urlparse.parse_qsl(base64.b64decode(str(event['body'])).decode('ascii')))
 
     webhook_url = get_slack_url()
     if webhook_url is None:
-        raise "Missing webhook URL"
+        raise Exception("Missing Slack URL")
 
-    channel = "#%s" % body["channel_name"]
+    # channel = "#%s" % body["channel_name"]
+
+    channel = "#zach-test"
 
     i = get_iterator()
-    m = get_value('S', "insults#count")
 
     # Ensure that messages are always in place. Shuffle to keep things interesting.
     if i < 1:
         create_messages()
 
+    m = get_value('S', "insults#count", 'N')
+
     # Reset the loop.
-    if i > m-1:
+    if i > (int(m) - 1):
         reset_iterator()
     else:
         increment_iterator()
@@ -202,3 +207,6 @@ def handle(event, context):
         },
         "body": ""
     }
+
+
+handle("foo", "foo")
